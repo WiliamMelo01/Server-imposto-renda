eb1c452d3e8faeffe8a31a6cf75dbce66e78e50e
WiliamMeloMota <102825885+WiliamMeloMota@users.noreply.github.com>
2023-04-03 15:17:20 -0300

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 env.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
